ff80f2e829fdb447685ff791ee4115693abc8752
Jason-Young-AI <AI.Jason.Young@outlook.com>
2025-01-03 18:54:59 +0800

    Sync

--- younger_apps_dl/commands/apps.py
+++ younger_apps_dl/commands/apps.py
@@ -21,47 +21,6 @@ def run(arguments):
 def deep_learning_run(arguments):
     pass
 
-
-def deep_learning_train_run(arguments):
-    task_name = arguments.task_name
-    config_filepath = pathlib.Path(arguments.config_filepath)
-    checkpoint_dirpath = pathlib.Path(arguments.checkpoint_dirpath)
-
-    from younger.applications.deep_learning import train
-
-    train(
-        task_name,
-        config_filepath,
-
-        checkpoint_dirpath,
-        arguments.checkpoint_name,
-        arguments.keep_number,
-
-        arguments.train_batch_size,
-        arguments.valid_batch_size,
-        arguments.shuffle,
-
-        arguments.checkpoint_filepath,
-        arguments.reset_optimizer,
-        arguments.reset_period,
-
-        arguments.life_cycle,
-        arguments.report_period,
-        arguments.update_period,
-        arguments.train_period,
-        arguments.valid_period,
-
-        arguments.device,
-        arguments.world_size,
-        arguments.master_addr,
-        arguments.master_port,
-        arguments.master_rank,
-
-        arguments.seed,
-        arguments.make_deterministic,
-    )
-
-
 def deep_learning_test_run(arguments):
     task_name = arguments.task_name
     config_filepath = pathlib.Path(arguments.config_filepath)
@@ -112,38 +71,6 @@ def set_applications_deep_learning_arguments(parser: argparse.ArgumentParser):
     subparser = parser.add_subparsers()
 
     train_parser = subparser.add_parser('train')
-    train_parser.add_argument('--task-name', type=str)
-    train_parser.add_argument('--config-filepath', type=str)
-
-    train_parser.add_argument('--checkpoint-dirpath', type=str)
-    train_parser.add_argument('--checkpoint-name', type=str, default='checkpoint')
-    train_parser.add_argument('--keep-number', type=int, default=50)
-
-    train_parser.add_argument('--train-batch-size', type=int, default=32)
-    train_parser.add_argument('--valid-batch-size', type=int, default=32)
-    train_parser.add_argument('--shuffle', action='store_true')
-
-    train_parser.add_argument('--checkpoint-filepath', type=str, default=None)
-    train_parser.add_argument('--reset-optimizer', action='store_true')
-    train_parser.add_argument('--reset-period', action='store_true')
-
-    train_parser.add_argument('--life-cycle', type=int, default=100)
-    train_parser.add_argument('--report-period', type=int, default=100)
-    train_parser.add_argument('--update-period', type=int, default=1)
-    train_parser.add_argument('--train-period', type=int, default=1000)
-    train_parser.add_argument('--valid-period', type=int, default=1000)
-
-    train_parser.add_argument('--device', type=str, choices=['CPU', 'GPU'], default='GPU')
-
-    train_parser.add_argument('--world-size', type=int, default=1)
-    train_parser.add_argument('--master-addr', type=str, default='localhost')
-    train_parser.add_argument('--master-port', type=str, default='16161')
-    train_parser.add_argument('--master-rank', type=int, default=0)
-
-    train_parser.add_argument('--seed', type=int, default=1234)
-    train_parser.add_argument('--make-deterministic', action='store_true')
-    train_parser.set_defaults(run=deep_learning_train_run)
-
     test_parser = subparser.add_parser('test')
     test_parser.add_argument('--task-name', type=str)
     test_parser.add_argument('--config-filepath', type=str)
--- younger_apps_dl/commands/standard.py
+++ younger_apps_dl/commands/standard.py
@@ -6,7 +6,7 @@
 # Author: Jason Young (杨郑鑫).
 # E-Mail: AI.Jason.Young@outlook.com
 # Last Modified by: Jason Young (杨郑鑫)
-# Last Modified time: 2024-12-31 16:13:29
+# Last Modified time: 2025-01-03 09:56:59
 # Copyright (c) 2024 Yangs.AI
 # 
 # This source code is licensed under the Apache License 2.0 found in the
@@ -26,5 +26,28 @@ def standard():
 
 
 @standard.group(name='train')
+@click.option('--task-name', type=str)
+@click.option('--config-filepath', type=str)
+@click.option('--checkpoint-dirpath', type=str)
+@click.option('--checkpoint-name', type=str, default='checkpoint')
+@click.option('--keep-number', type=int, default=50)
+@click.option('--train-batch-size', type=int, default=32)
+@click.option('--valid-batch-size', type=int, default=32)
+@click.option('--shuffle', action='store_true')
+@click.option('--checkpoint-filepath', type=str, default=None)
+@click.option('--reset-optimizer', action='store_true')
+@click.option('--reset-period', action='store_true')
+@click.option('--life-cycle', type=int, default=100)
+@click.option('--report-period', type=int, default=100)
+@click.option('--update-period', type=int, default=1)
+@click.option('--train-period', type=int, default=1000)
+@click.option('--valid-period', type=int, default=1000)
+@click.option('--device', type=str, choices=['CPU', 'GPU'], default='GPU')
+@click.option('--world-size', type=int, default=1)
+@click.option('--master-addr', type=str, default='localhost')
+@click.option('--master-port', type=str, default='16161')
+@click.option('--master-rank', type=int, default=0)
+@click.option('--seed', type=int, default=1234)
+@click.option('--make-deterministic', action='store_true')
 def standard_train():
     pass
